1dbeff614eb565ab57619bcd3ea6759ac0ff1f4d
ajitashwath <ajitashwathr23@lpu.in>
2025-07-13 12:18:05 +0530

    changed langchain

--- app.py
+++ app.py
@@ -1,9 +1,9 @@
 from flask import Flask, request, jsonify, render_template
 from PyPDF2 import PdfReader
-from langchain.vectorstores.cassandra import Cassandra
+from langchain_community.vectorstores import Cassandra
 from langchain.indexes.vectorstore import VectorStoreIndexWrapper
-from langchain.llms import OpenAI
-from langchain.embeddings import OpenAIEmbeddings
+from langchain_community.llms import OpenAI
+from langchain_community.embeddings import OpenAIEmbeddings
 from langchain.text_splitter import CharacterTextSplitter
 from werkzeug.utils import secure_filename
 from dotenv import load_dotenv
